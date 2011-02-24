67c496aa149aa347a58df5d1fc4aa98c2a66fc89
Thiago Jackiw <thiago@aurorafeint.com>
2011-02-23 18:16:41 -0800

    Renaming the hash method from prune to cleanse

--- lib/hivelet/core_ext.rb
+++ lib/hivelet/core_ext.rb
@@ -5,9 +5,25 @@ class Object
 end
 
 class Hash
-  def prune
+  # Recursively removes nil and empty values from a hash and returns a new clean hash.
+  # 
+  # hash = { 
+  #   :name => nil, 
+  #   :country => 'US', 
+  #   :preferences => {
+  #     :quality => nil,
+  #     :sites => {
+  #       :small => [],
+  #       :medium => []
+  #     }
+  #   } 
+  # }
+  # hash.cleanse
+  # => {:country=>"US"}
+  # 
+  def cleanse
     self.each do |key, value|
-      value.prune if value.is_a?(Hash)
+      value.cleanse if value.is_a?(Hash)
       delete(key) if value.to_s.empty?
     end
   end
